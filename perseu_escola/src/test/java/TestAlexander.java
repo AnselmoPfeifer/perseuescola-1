0580141e4136ecbf482bba08f94aea4db9f5fcbd
Alexander Aleyne <aleyne29@hotmail.com>
2014-11-26 21:03:41 -0400

    Estou apenas testando o sincronismo

---
+++ perseu_escola/src/test/java/TestAlexander.java
@@ -0,0 +1,9 @@
+
+public class TestAlexander {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("TESTANDO COMMIT E PUSH !!!!");
+	}
+
+}
