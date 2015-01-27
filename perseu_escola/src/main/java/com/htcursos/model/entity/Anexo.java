c7abf7f645c9526975eab9a1e0147b0e7f1e4ff9
Alison Moura <alison.oghino@gmail.com>
2015-01-27 16:02:19 -0300

    Terminando a modelagem da entidade Anexo

--- perseu_escola/src/main/java/com/htcursos/model/entity/Anexo.java
+++ perseu_escola/src/main/java/com/htcursos/model/entity/Anexo.java
@@ -1,5 +1,34 @@
 package com.htcursos.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+
+@Entity
 public class Anexo {
+
+	@Id
+	@SequenceGenerator(name = "seq_anexo", sequenceName = "seq_anexo", allocationSize = 1, initialValue = 30)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_anexo")
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn
+	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn
+	private Matricula matricula;
+	//CAMINHO DA PASTA UPLOAD
+	private String caminho;
+	//NOME GERADO
+	private String nomeGerado;
+	//NOME ORIGINAL DO ARQUIVO
+	private String tituloOriginal;
 
 }
